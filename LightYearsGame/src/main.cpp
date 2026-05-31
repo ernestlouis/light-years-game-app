b7b774c0bc670fc46bc51db83161eb8c077b1121
ernestlouis <ernestlouisx@hotmail.com>
2026-05-31 16:19:31 -0400

    Automate runtime DLL copying and remove manual SFML library copying

--- LightYearsGame/src/main.cpp
+++ LightYearsGame/src/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    sf::RenderWindow window(
+        sf::VideoMode(800, 600),
+        "MyWindow"
+    );
+
     std::cout << "Light Years Game Started!" << std::endl;
 
-    return 0;
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+
 }
