1e637e17130a818d0f348390dcdc52c5eee81a40
ernestlouis <ernestlouisx@hotmail.com>
2026-06-03 21:27:13 -0400

    feat: implement game loop and rendering framework
    - Add fixed timestep game loop
    - Add Tick and Render lifecycle functions
    - Add internal tick and render handlers
    - Add SFML window management
    - Add frame timing with sf::Clock
    - Render initial test shape to window

--- LightYearsEngine/include/framework/Application.h
+++ LightYearsEngine/include/framework/Application.h
@@ -10,7 +10,15 @@ namespace ly
 		void Run();
 
 	private:
+		void Tick_Internal(float delta_time);
+		void Render_Internal();
+
+		virtual void Render();
+		virtual void Tick(float delta_time);
+
 		sf::RenderWindow m_window;
+		float m_target_frame_rate;
+		sf::Clock m_tick_clock;
 		
 	};
 	
--- LightYearsEngine/src/framework/Application.cpp
+++ LightYearsEngine/src/framework/Application.cpp
@@ -1,14 +1,21 @@
+#include <iostream>
 #include "framework/Application.h"
 
 namespace ly
 {
-	Application::Application()
-		:m_window{ sf::VideoMode(800, 600),"MyWindow" }
+    Application::Application()
+        :m_window{ sf::VideoMode(800, 600),"MyWindow" },
+        m_target_frame_rate{ 60.f },
+        m_tick_clock{}
 	{
 	}
 
 	void Application::Run()
 	{
+        m_tick_clock.restart();
+        float accumulated_time = 0.f;
+        float target_delta_time = 1.f / m_target_frame_rate;
+
         while (m_window.isOpen())
         {
             sf::Event event;
@@ -21,9 +28,46 @@ namespace ly
                 }
             }
 
-            m_window.clear(sf::Color::Black);
-            m_window.display();
+            
+
+            accumulated_time += m_tick_clock.restart().asSeconds();
+            while (accumulated_time > target_delta_time)
+            {
+                accumulated_time -= target_delta_time;
+                Tick_Internal(target_delta_time);
+                Render_Internal();
+            }
         }
 	}
 
+    void Application::Tick_Internal(float delta_time)
+    {
+        Tick(delta_time);
+    }
+
+    void Application::Render_Internal()
+    {
+        m_window.clear(sf::Color::Black);
+
+        Render();
+
+        m_window.display();
+    }
+
+    void Application::Render()
+    {
+        sf::CircleShape circ{ 50 };
+        circ.setFillColor(sf::Color::Green);
+        circ.setOrigin(50, 50);
+        circ.setPosition(m_window.getSize().x / 2.f, m_window.getSize().y / 2.f);
+
+        m_window.draw(circ);
+    }
+
+    void Application::Tick(float delta_time)
+    {
+        std::cout << "Ticking at framerate: " << 1.f / delta_time << std::endl;
+    }
+
+   
 }
